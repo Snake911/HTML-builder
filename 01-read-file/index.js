e92a9a12a2e1733618ad40f06e380f238448cde5
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-12 12:44:54 +0300

    feat: make 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+
+const fd = fs.createReadStream(path.join(__dirname, 'text.txt'));
+fd.on('data', (data) => {
+  stdout.write(data);
+});
