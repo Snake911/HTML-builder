d431dfbfa0d1152d75e1cbe334cce98d56974ac3
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-13 13:05:20 +0300

    fix: add remove bundle.css before rebuilding

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
     .then((files) => {
         const styles = files.filter(file => file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css');
+        fs.promises.rm(path.join(__dirname, 'project-dist/bundle.css'), {force: true});
         styles.map(style => {
             fs.promises.readFile(path.join(__dirname, 'styles', style.name))
                 .then(css => {
