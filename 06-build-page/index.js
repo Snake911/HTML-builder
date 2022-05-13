f92f9b6b337773667e46b4e7c8d44063b60efef8
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-13 11:10:34 +0300

    feat: make 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,12 +1,58 @@
 const fs = require('fs');
 const path = require('path');
 
+const buildDir = 'project-dist';
+
 (async function build() {
-    const template = (await fs.promises.readFile(path.join(__dirname, 'template.html'))).toString();
-    if(template.indexOf('{{') !== -1) {
+    // Собираем шаблон
+    await fs.promises.mkdir(path.join(__dirname, buildDir), {recursive: true});
+    let template = (await fs.promises.readFile(path.join(__dirname, 'template.html'))).toString();
+    while(template.indexOf('{{') !== -1) {
         const component = template.match(/{{(.*)}}/)[1];
         const componentCode = (await fs.promises.readFile(path.join(__dirname, 'components', component + '.html'))).toString();
-        template.replace(/{{component}}/, componentCode);
-        console.log(template);
+        const compNameReg = new RegExp(`{{${component}}}`);
+        template = template.replace(compNameReg, componentCode);
     }
-})()
+    fs.writeFile(
+        path.join(__dirname, buildDir, 'index.html'),
+        template,
+        (err) => {
+            if (err) throw err;
+        }
+    );
+
+    // Собираем стили
+    fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+        .then((files) => {
+            const styles = files.filter(file => file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css');
+            styles.map(style => {
+                fs.promises.readFile(path.join(__dirname, 'styles', style.name))
+                    .then(css => {
+                        fs.appendFile(
+                            path.join(__dirname, buildDir, 'style.css'),
+                            css.toString() + '\n',
+                            (err) => {
+                                if (err) throw err;
+                            }
+                        );
+                    });
+            });
+        });
+
+    // Собираем ассеты
+    (async function copyDir(dir) {
+        await fs.promises.mkdir(path.join(__dirname, buildDir, dir), {recursive: true});
+        fs.promises.readdir(path.join(__dirname, dir), {withFileTypes: true})
+            .then(entities => { 
+                entities.map(entity => {
+                    if(entity.isFile()) {
+                        fs.promises.copyFile(path.join(__dirname, dir, entity.name), path.join(__dirname, buildDir, dir, entity.name));
+                    } else {
+                        copyDir(path.join(dir, entity.name));
+                    }               
+                });
+            });
+    })('assets');
+})();
+
+
