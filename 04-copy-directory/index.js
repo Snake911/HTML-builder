c0aa5ce6cb9d8f30722b5a0e358268de711f9dc0
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-25 16:26:30 +0300

    fix: add create folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,8 +3,14 @@ const path = require('path');
 
 (async function copyDir(dir) {
     const copyDirName = `${dir}-copy`;
-    await fs.promises.rm(path.join(__dirname, copyDirName), {recursive: true});
+
     await fs.promises.mkdir(path.join(__dirname, copyDirName), {recursive: true});
-    fs.promises.readdir(path.join(__dirname, dir))
+
+    const files = await fs.promises.readdir(path.join(__dirname, copyDirName));
+    for (const file of files) {
+      await fs.promises.unlink(path.join(__dirname, copyDirName, file));
+    }
+    
+    await fs.promises.readdir(path.join(__dirname, dir))
         .then(files => files.map(file => fs.promises.copyFile(path.join(__dirname, dir, file), path.join(__dirname, copyDirName, file))));    
 })('files');
